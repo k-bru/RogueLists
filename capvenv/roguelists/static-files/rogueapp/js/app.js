5530f5223c4c9203bc7cb586044742f1e84ee2d0
k-bru <keegan.brunmeier@gmail.com>
2023-04-29 02:11:15 -0400

    js fix and some grammar changes

--- capvenv/roguelists/static-files/rogueapp/js/app.js
+++ capvenv/roguelists/static-files/rogueapp/js/app.js
@@ -1,31 +1,7 @@
-/**
- * Adds event listeners and initializes the page on DOMContentLoaded.
- * 
- * Initializes the page by showing collapsed headers with the CSS selector '.list-holder h2.collapsed'
- * and hiding collapsed headers with the CSS selector '.sort-holder h2.show'.
- * 
- * Adds a change event listener to each select dropdown with the name 'tier_rank'. When a user changes
- * the dropdown value, the form closest to the dropdown is submitted.
- */
-document.addEventListener("DOMContentLoaded", function () {
-  // Show collapsed headers that are initially hidden.
-  showCollapsedHeaders('.list-holder h2.collapsed');
-
-  // Hide collapsed headers that are initially displayed.
-  hideCollapsedHeaders('.sort-holder h2.show');
-
-  // Add change event listeners to 'tier_rank' select dropdowns.
-  document.querySelectorAll('select[name="tier_rank"]').forEach(function (select) {
-    select.addEventListener('change', function () {
-      // Submit the form closest to the dropdown when the value is changed
-      this.closest('form').submit();
-    });
-  });
-});
 
 /**
-  Displays collapsed headers for the specified selector. Used for areas that are shown by default.
-  @param {string} selector - The CSS selector for the collapsed headers. 
+ Displays collapsed headers for the specified selector. Used for areas that are shown by default.
+ @param {string} selector - The CSS selector for the collapsed headers. 
  */
 function showCollapsedHeaders(selector) {
   const collapsedHeaders = document.querySelectorAll(selector);
@@ -36,8 +12,8 @@ function showCollapsedHeaders(selector) {
 }
 
 /**
-  Hides collapsed headers for the specified selector. Used for areas that are collapsed by default.
-  @param {string} selector - The CSS selector for the collapsed headers.
+ Hides collapsed headers for the specified selector. Used for areas that are collapsed by default.
+ @param {string} selector - The CSS selector for the collapsed headers.
  */
 function hideCollapsedHeaders(selector) {
   const collapsedHeaders = document.querySelectorAll(selector);
@@ -49,31 +25,31 @@ function hideCollapsedHeaders(selector) {
 
 /**
  * Function to go back to previous page in browser history
- */
+*/
 function goBack() {
   window.history.back();
 }
 
 /**
  * Function to add event listeners to the edit name and description icons and toggle their visibility
- */
+*/
 function addEditNameAndDescriptionListeners() {
   const editNameIcon = document.getElementById('edit-name-icon');
   const editDescriptionIcon = document.getElementById('edit-description-icon');
   const nameInputContainer = document.getElementById('name-input-container');
   const descriptionInputContainer = document.getElementById('description-input-container');
-
+  
   editNameIcon.addEventListener('click', () => {
     nameInputContainer.classList.toggle('d-none');
   });
-
+  
   editDescriptionIcon.addEventListener('click', () => {
     descriptionInputContainer.classList.toggle('d-none');
   });
 }
 /**
  *  When the user scrolls down 20px from the top of the document, show the button
- */
+*/
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("back-to-top").style.display = "block";
@@ -84,7 +60,7 @@ function scrollFunction() {
 
 /**
  * When the user clicks on the button, scroll to the top of the document
- */
+*/
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -110,6 +86,31 @@ function searchUsers() {
   });
 }
 
+/**
+ * Adds event listeners and initializes the page on DOMContentLoaded.
+ * 
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  // Show collapsed headers that are initially hidden.
+  showCollapsedHeaders('.list-holder h2.collapsed');
+
+  // Hide collapsed headers that are initially displayed.
+  hideCollapsedHeaders('.sort-holder h2.show');
+  
+  // Allow for edits of list names and descriptions.
+  addEditNameAndDescriptionListeners();
+
+  // Update search results on users page
+  searchUsers();
+
+  // Add change event listeners to 'tier_rank' select dropdowns.
+  document.querySelectorAll('select[name="tier_rank"]').forEach(function (select) {
+    select.addEventListener('change', function () {
+      // Submit the form closest to the dropdown when the value is changed
+      this.closest('form').submit();
+    });
+  });
+});
+
+//Back to top button
 window.onscroll = function () { scrollFunction() };
-searchUsers();
-addEditNameAndDescriptionListeners();
